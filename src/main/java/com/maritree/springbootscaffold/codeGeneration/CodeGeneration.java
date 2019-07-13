c0264d8847647b89039f541eef11523b196fa198
edelbay <edelbay@qq.com>
2019-07-13 09:58:52 +0800

    update

--- src/main/java/com/maritree/springbootscaffold/codeGeneration/CodeGeneration.java
+++ src/main/java/com/maritree/springbootscaffold/codeGeneration/CodeGeneration.java
@@ -1,99 +1,99 @@
-package com.maritree.springbootscaffold.codeGeneration;
-
-import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.config.*;
-import com.baomidou.mybatisplus.generator.config.rules.DbType;
-import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-
-/**
- * @author cheng
- * @ClassName: CodeGeneration
- * @Description: 代码生成器
- * @date 2018年1月25日 下午2:55:14
- */
-public class CodeGeneration {
-
-    /**
-     * @param args
-     * @Title: main
-     * @Description: 生成
-     */
-    public static void main(String[] args) {
-        AutoGenerator mpg = new AutoGenerator();
-
-        // 全局配置
-        GlobalConfig gc = new GlobalConfig();
-        gc.setOutputDir("E://code");
-        gc.setFileOverride(true);
-        gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
-        gc.setEnableCache(false);// XML 二级缓存
-        gc.setBaseResultMap(true);// XML ResultMap
-        gc.setBaseColumnList(false);// XML columList
-        gc.setAuthor("maritree");// 作者
-
-        // 自定义文件命名，注意 %s 会自动填充表实体属性！
-        gc.setControllerName("%sController");
-        gc.setServiceName("%sService");
-        gc.setServiceImplName("%sService");
-        gc.setMapperName("%sMapper");
-        gc.setXmlName("%sMapper");
-
-
-        mpg.setGlobalConfig(gc);
-
-        // 数据源配置
-        DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setDbType(DbType.MYSQL);
-        dsc.setDriverName("com.mysql.jdbc.Driver");
-        dsc.setUsername("root");
-        dsc.setPassword("root");
-        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/authority");
-        mpg.setDataSource(dsc);
-
-        // 策略配置
-        StrategyConfig strategy = new StrategyConfig();
-        strategy.setTablePrefix(new String[]{"sys_"});// 此处可以修改为您的表前缀
-        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
-        strategy.setInclude(); // 需要生成的表
-
-        strategy.setSuperServiceClass(null);
-        strategy.setSuperServiceImplClass(null);
-        strategy.setSuperMapperClass(null);
-        strategy.setRestControllerStyle(true);
-
-        mpg.setStrategy(strategy);
-
-        // 包配置
-        PackageConfig pc = new PackageConfig();
-        pc.setParent("com.maritree.springbootscaffold.buss");
-        pc.setController("controller");
-        pc.setService("service");
-        pc.setServiceImpl("service");
-        pc.setMapper("mapper");
-        pc.setEntity("entity");
-        pc.setXml("xml");
-        mpg.setPackageInfo(pc);
-
-
-        TemplateConfig tc = new TemplateConfig();
-//        tc.setXml(null);
+//package com.maritree.springbootscaffold.codeGeneration;
+//
+//import com.baomidou.mybatisplus.annotation.DbType;
+//import com.baomidou.mybatisplus.core.config.GlobalConfig;
+//import com.baomidou.mybatisplus.generator.AutoGenerator;
+//
+//
+///**
+// * @author cheng
+// * @ClassName: CodeGeneration
+// * @Description: 代码生成器
+// * @date 2018年1月25日 下午2:55:14
+// */
+//public class CodeGeneration {
+//
+//    /**
+//     * @param args
+//     * @Title: main
+//     * @Description: 生成
+//     */
+//    public static void main(String[] args) {
+//        AutoGenerator mpg = new AutoGenerator();
+//
+//        // 全局配置
+//        GlobalConfig gc = new GlobalConfig();
+//        gc.setOutputDir("d://code");
+//        gc.setFileOverride(true);
+//        gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
+//        gc.setEnableCache(false);// XML 二级缓存
+//        gc.setBaseResultMap(true);// XML ResultMap
+//        gc.setBaseColumnList(false);// XML columList
+//        gc.setAuthor("maritree");// 作者
+//
+//        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+//        gc.setControllerName("%sController");
+//        gc.setServiceName("%sService");
+//        gc.setServiceImplName("%sService");
+//        gc.setMapperName("%sMapper");
+//        gc.setXmlName("%sMapper");
+//
+//
+//        mpg.setGlobalConfig(gc);
+//
+//        // 数据源配置
+//        DataSourceConfig dsc = new DataSourceConfig();
+//        dsc.setDbType(DbType.MYSQL);
+//        dsc.setDriverName("com.mysql.jdbc.Driver");
+//        dsc.setUsername("root");
+//        dsc.setPassword("example");
+//        dsc.setUrl("jdbc:mysql://115.159.84.245:3306/authority?useUnicode=true&characterEncoding=utf-8&useSSL=false");
+//        mpg.setDataSource(dsc);
+//
+//        // 策略配置
+//        StrategyConfig strategy = new StrategyConfig();
+//        strategy.setTablePrefix(new String[]{"sys_"});// 此处可以修改为您的表前缀
+//        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+//        strategy.setInclude(); // 需要生成的表
+//
+//        strategy.setSuperServiceClass(null);
+//        strategy.setSuperServiceImplClass(null);
+//        strategy.setSuperMapperClass(null);
+//        strategy.setRestControllerStyle(true);
+//
+//        mpg.setStrategy(strategy);
+//
+//        // 包配置
+//        PackageConfig pc = new PackageConfig();
+//        pc.setParent("com.maritree.springbootscaffold.buss");
+//        pc.setController("controller");
+//        pc.setService("service");
+//        pc.setServiceImpl("service");
+//        pc.setMapper("mapper");
+//        pc.setEntity("entity");
+//        pc.setXml("xml");
+//        mpg.setPackageInfo(pc);
+//
+//
+//        TemplateConfig tc = new TemplateConfig();
+////        tc.setXml(null);
+////        mpg.setTemplate(tc);
+//
+//        // 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/templates 下面内容修改，
+//        // 放置自己项目的 src/main/resources/templates 目录下, 默认名称一下可以不配置，也可以自定义模板名称
+//        // TemplateConfig tc = new TemplateConfig();
+////         tc.setController();
+//        // tc.setEntity("...");
+//        // tc.setMapper("...");
+//        // tc.setXml("...");
+//        tc.setService(null);
+//        // tc.setServiceImpl("...");
+//        // 如上任何一个模块如果设置 空 OR Null 将不生成该模块。
 //        mpg.setTemplate(tc);
-
-        // 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/templates 下面内容修改，
-        // 放置自己项目的 src/main/resources/templates 目录下, 默认名称一下可以不配置，也可以自定义模板名称
-        // TemplateConfig tc = new TemplateConfig();
-//         tc.setController();
-        // tc.setEntity("...");
-        // tc.setMapper("...");
-        // tc.setXml("...");
-        tc.setService(null);
-        // tc.setServiceImpl("...");
-        // 如上任何一个模块如果设置 空 OR Null 将不生成该模块。
-        mpg.setTemplate(tc);
-
-        // 执行生成
-        mpg.execute();
-
-    }
-
-}
+//
+//        // 执行生成
+//        mpg.execute();
+//
+//    }
+//
+//}
